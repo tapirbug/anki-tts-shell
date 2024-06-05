057fa9bc88edb38a61593edb27a619e10d1a056a
Philipp Stadler <hello@phstadler.com>
2024-06-05 13:31:01 +0200

    feat: add anki-tts-shell.json config

--- README.md
+++ README.md
@@ -1,18 +1,25 @@
 # Anki TTS Speech Dispatcher
-Adding speech-dispatcher support to Anki through an add-on. It uses `spd-say` to speak text, so if that works then this add-on should work.
+Add speech synthesis support via CLI tool to Anki.
 
-This add-on is new and hasn't been extensively test. It assumes you have a properly configured speech-dispatcher and that `spd-say` works (this add-on uses the default voice). Some limitations which I hope to fix:
-1. "en_US" is hardcoded into the add-on though isn't used. If speechd is configured to use another language, it should work, but specifying different languages in the field should have no effect e.g. `{{tts fr_FR:SomeField}}`
-2. Field TTS speed control is not yet supported e.g. `{{tts fr_FR speed=0.8:SomeField}}`
-3. config file - add variable so reading can be disabled while reviewing
+You can configure it to use any CLI tool, language or voice you like, for example `spd-say` or `ekho`.
 
-Speech Dispatcher is configured using separate config files. It allows for many customizations including different speech settings for different applications (Firefox, Emacs, and now Anki). It works with many TTS engines. If an engine is not already supported, the process of making a new one is relatively simple using the "generic output module". See the Configuration section of the speech-dispatcher Texinfo document for file locations and examples.
+## Configuring voices
+To add support for a new language, provide a configuration file
 
-## Multiple voices
-According to Anki's manual, multiple voices can be specified and Anki should use the first available one:
-`{{tts en_US voices=speechd,HyperTTS,gTTS,Apple_Otoya,Microsoft_Haruka:FieldName}}`
+If you want to use a different tool like [ekho](https://eguidedog.net/ekho.php)
+instead of `spd-say`, provide a configuration file in `~/.config/anki-tts-shell.json`.
 
-Note: AnkiDroid doesn't yet support this sytax 
+For each language, provide a language code, and a command that can speak the text if it is written to the programs stdin:
+```
+{
+    "voices": [
+        {
+            "code": "zh_CN",
+            "cmd": [ "ekho", "--file=-" ]
+        }
+    ]
+}
+```
 
-## Files
-`old-attempt/` contains the first iteration which used the speechd python bindings. The current version uses subprocess and spd-say because, atleast on Debian 11.6, a separate package was required (python3-speechd). Future versions may return to the python bindings implementation.
+## Thanks
+This add-on is based on the work of subdomain on https://github.com/sudomain/anki-TTS-speech-dispatcher
--- __init__.py
+++ __init__.py
@@ -1,43 +1,30 @@
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
-"""
-Speech Dispatcher is configured using separate config files. It allows for many customizations including different speech settings for different applications (Firefox, Emacs, and now Anki). It works with many TTS engines. If an engine is not already supported, the process of making a new one is relatively simple using the "generic output module". See the Configuration section of the speech-dispatcher Texinfo document for file locations and examples.
-
-Heavily based on MacTTSPlayer(TTSProcessPlayer): https://github.com/ankitects/anki/blob/f9126927b1c537f76b2e11f2f8a6003d2313245b/qt/aqt/tts.py#L170
-"""
-
-# `info spd-say` should have most of the information needed to complete this TODO list, I just need more time
-#TODO spd-say --pipe-mode might give better performance since it will keep an instance of spd-say reading from stdin and we can write to that using subprocess. There's an example in old-attempts/ directory
-#TODO list voices with spd-say -L and parsing the output tab separated value of "NAME LANGUAGE VARIANT" however we need to account for NAME containing spaces such as "Chinese (Mandarin)"
-#TODO remove hardcoded language "en_US" using spd-say --language 
-#TODO test stopping with an output module known to support it such as espeak
-#TODO remove unnecesary imports
-#TODO support per field TTS speed e.g. {{tts fr_FR speed=0.8:SomeField}}. spd-say --rate can take values between -100 and 100 so the decimal values Anki uses must be normalizaed to integers between -100 and 100 
-
 import subprocess
 from dataclasses import dataclass
-from typing import List, cast
+from typing import List
+from os.path import expanduser
 
 from anki.sound import AVTag, TTSTag
 from aqt import mw
-from aqt.sound import OnDoneCallback, av_player
+from aqt.sound import av_player
 
 import aqt.tts
+import json
 
 # we subclass the default voice object to store the speechd language code
 @dataclass
-class SpeechDVoice(aqt.tts.TTSVoice):
-    #TODO Getting the voice list from `spd-say --list-synthesis-voices` can take several seconds if many are installed, so do it at Anki startup
-    speechd_lang: str
+class CliVoice(aqt.tts.TTSVoice):
+    cmd: list[str]
 
-#class SpeechDPlayer(TTSProcessPlayer):
-class SpeechDPlayer(aqt.tts.TTSProcessPlayer):
+#class CliVoicePlayer(TTSProcessPlayer):
+class CliVoicePlayer(aqt.tts.TTSProcessPlayer):
     # this is called the first time Anki tries to play a TTS file
     def get_available_voices(self) -> List[aqt.tts.TTSVoice]:
         voices = []
-
-        # add the voice using the name "speechd"
-        voices.append(SpeechDVoice(name="speechd", lang="en_US", speechd_lang="en"))
+        with open(expanduser("~/.config/anki-tts-shell.json"), "r") as config_file:
+            for voice in json.loads(config_file.read())["voices"]:
+                voices.append(CliVoice(**voice))
         return voices  # type: ignore
 
     # this is called on a background thread, and will not block the UI
@@ -45,24 +32,9 @@ class SpeechDPlayer(aqt.tts.TTSProcessPlayer):
         #get the avtag
         assert isinstance(tag, TTSTag)
         match = self.voice_for_tag(tag)
-
-        # is the field blank?
-        if not tag.field_text.strip():
-            return
-
-        subprocess.run(["spd-say", "--wait", "--application-name", "Anki", tag.field_text])
-        return
-
-    # this is called on the main thread, after _play finishes
-    # It seems it's only really necessary if the intention is to create temporary files. Instead, this add-on uses a subprocess.
-    #def _on_done(self, ret: Future, cb: OnDoneCallback) -> None:
-        #pass
-
-    # we don't support stopping while the file is being downloaded
-    # (but the user can interrupt playing after it has been downloaded)
-    def stop(self):
-        pass
-
+        text = tag.field_text.strip()
+        if match and hasattr(match.voice, "cmd") and text:
+            subprocess.run(match.voice.cmd, input=text.encode('utf-8'))
 
 # register our handler
-av_player.players.append(SpeechDPlayer(mw.taskman))
+av_player.players.append(CliVoicePlayer(mw.taskman))
